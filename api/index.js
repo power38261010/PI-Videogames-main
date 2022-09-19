3bc453d65a84bf3cf5794fe4bf600bb05185e707
Arrua Alejandro <94117716+power38261010@users.noreply.github.com>
2022-09-19 16:13:39 -0300

    sync false

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const {loadGeneros} = require("./src/Middleware/loadGenres");
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
 
     server.listen(process.env.PORT, () => {
         console.log('%s listening at 3001');
