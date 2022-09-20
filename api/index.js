068c1a5aa588769924044404ba67a0606758c598
Arrua Alejandro <94117716+power38261010@users.noreply.github.com>
2022-09-19 22:27:36 -0300

    es

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const {loadGeneros} = require("./src/Middleware/loadGenres");
-const port = process.env.PORT ||3001
+const PORT = process.env.PORT ||3001
 
 conn.sync({ force: false }).then(() => {
 
-    server.listen(port, () => {
-        console.log('%s listening at 3001');
+    server.listen(PORT, () => {
+        console.log(`%s listening at port ${ PORT }`);
 		loadGeneros();
   
     });
